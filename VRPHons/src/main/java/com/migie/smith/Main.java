f998f19edf766103ae833e82a044a433397399f4
migiesmith <migiesmith@gmail.com>
2016-11-09 22:11:14 +0000

    Initial commit of an empty project connected to the current system

---
+++ VRPHons/src/main/java/com/migie/smith/Main.java
@@ -0,0 +1,44 @@
+package com.migie.smith;
+
+
+import agent.auctionSolution.MapServer;
+import agent.auctionSolution.MyAuctioneer;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+public class Main{
+	
+
+    public static void main( String[] args )
+    {	
+		Runtime myRuntime = Runtime.instance();
+
+		// prepare the settings for the platform that we're going to start
+		Profile myProfile = new ProfileImpl();
+		// Increase the maximum results from the DF (yellow-pages)
+		myProfile.setParameter("jade_domain_df_maxresult","1000");
+
+		// create the main container
+		ContainerController myContainer = myRuntime.createMainContainer(myProfile);		
+		
+		try {
+			//JADE gui
+		    AgentController rma = myContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
+		    rma.start();
+		    
+		    
+		    AgentController auctioneer = myContainer.createNewAgent("Auctioneer", MyAuctioneer.class.getCanonicalName(), null);
+		    auctioneer.start();
+		    
+		    AgentController mapServer = myContainer.createNewAgent("Map-Server", MapServer.class.getCanonicalName(), null);
+		    mapServer.start();
+		} catch(StaleProxyException e) {
+		    e.printStackTrace();
+		}
+        
+    }
+}
